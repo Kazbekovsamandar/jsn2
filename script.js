3ebd4b8d9ec869a2f4d97fdfd92874902041c4c6
Kazbekovsamandar <kazbekovs50@gmail.com>
2025-03-19 13:03:44 +0300

    string metod

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //alert confirm prompt
-//const message=alert('Hello World');
-//console.log(message);
+// const message=alert('Hello World');
+// console.log(message);
 //confirm inglizchadan tasdiqlash va ikki xil malumot
 //const age = confirm("are you 18+");
 //console.log(age);
@@ -304,26 +304,235 @@
 //}
 
 
-document.getElementById("mySelect").onchange = function() {
-  myFunction();
-};  
+//document.getElementById("mySelect").onchange = function() {
+ // myFunction();
+//};  
 
 // optionlardi ozgartirish onchangega misol  onchange faqat  
 
-function myFunction() {
-  let x = document.getElementById("mySelect").value;
-  document.getElementById("demo").innerHTML = "You selected: " + x;
-}
+//function myFunction() {
+  //let x = document.getElementById("mySelect").value;
+//  document.getElementById("demo").innerHTML = "You selected: " + x;
+//}
 //myfunction  optionlardi almashtirish uchun
 
 //
 
-document.getElementById("myInput").onchange = function() {
-  inputFunction();
-};
+//document.getElementById("myInput").onchange = function() {
+ // inputFunction();
+//};
 
-function inputFunction() {
-    let inputVal = document.getElementById("myInput").value;
-    document.getElementById("inputDemo").innerHTML = "You entered: " + inputVal;
+//function inputFunction() {
+   // let inputVal = document.getElementById("myInput").value;
+   // document.getElementById("inputDemo").innerHTML = "You entered: " + inputVal;
 
-}
+//}
+
+//let a=4;
+//let b=5;
+//let c=a+b;
+//console.log(c);
+
+// funksiya -> kodni qayta qayta yozmaslik
+// js funksiya , declaration,expression,arrow,
+// FUNCTION DECLERATION
+// 3 TA ASOSIY QISM  key->function nomi->
+
+//function  SHowHelloworld()
+//{
+  //  console.log("Hello world!");
+//}
+//SHowHelloworld();
+
+//let age=17;
+//console.log(age);
+//function sayHellotoUser(name)
+//{
+   // let age=16;
+  //  console.log(`Hello ${name},he is ${age} years old`);
+//}
+
+//sayHellotoUser("Muhammadaziz");
+//console.log(age);
+
+// () -> argument dynamic malumotlar
+
+// ""  ` ` -> backstick
+
+// argumentdi ichida name
+
+// lakal va global malumot turi
+
+// lokal malumot turi -> functiondi  blok sxemasi ichida boladi
+
+// global malumot turi -> functiondi blok sxemasi ichida bolmasa
+
+//let age=> 17 edi , funksiya ichida 20 bersak ham u tasir qilmadi ,funksiya ichida 20 tashqarida esa 17
+
+
+//function getCalculations(a,b)
+//{
+  //  return a+b;  // malumotdi qaytaradi ozida saqlab turadi, i ustida amal bajarsa boladi 
+   // ozidan keyin ishlatmidi 
+  //  let tyu=25;
+   // console.log(tyu);
+//}
+//const ae=18+(getCalculations(4,7));
+//console.log(ae);
+
+//function SHowHelloworld()
+//{
+ //   console.log("Hello world!");
+//}
+//SHowHelloworld();
+
+//let a=4;
+//let b=5;
+//let c=a+b;
+//console.log(c);
+
+// funksiya -> kodni qayta qayta yozmaslik
+// js funksiya , declaration,expression,arrow,
+// FUNCTION DECLERATION
+// 3 TA ASOSIY QISM  key->function nomi->
+
+//function  SHowHelloworld()
+//{
+ //   console.log("Hello world!");
+//}
+//SHowHelloworld();
+
+//let age=17;
+//console.log(age);
+//function sayHellotoUser(name)
+//{
+   // let age=20;
+  //  console.log(`Hello ${name},he is ${age} years old`);
+//}
+
+//sayHellotoUser("Sardor");
+//console.log(age);
+
+// () -> argument dynamic malumotlar
+
+// ""  ` ` -> backstick
+
+// argumentdi ichida name
+
+// lakal va global malumot turi
+
+// lokal malumot turi -> functiondi  blok sxemasi ichida boladi
+
+// global malumot turi -> functiondi blok sxemasi ichida bolmasa
+
+//let age=> 17 edi , funksiya ichida 20 bersak ham u tasir qilmadi ,funksiya ichida 20 tashqarida esa 17
+
+//const ae=18+(getCalculations(4,7));
+//console.log(ae);
+
+//function getCalculations(a,b)
+//{
+ //   return a+b;  // malumotdi qaytaradi ozida saqlab turadi, i ustida amal bajarsa boladi 
+   // ozidan keyin ishlatmidi 
+ //   let tyu=25;
+  //  console.log(tyu);
+//}
+
+
+// function decleration => biz uni script ichida xohlagan joyimizda chaqiramiz
+
+// const HelloWorld=function()
+// {
+//    console.log("Hello world!");
+// }
+// HelloWorld();
+//const CalculateXojiaka=function(a,b)
+//{
+  //  return a+b;
+//}
+//console.log(CalculateXojiaka(9,7));
+
+// function expression
+
+
+// arrow function =>
+
+//const CalculateXojiaka1=(a,b)=>
+//{
+ //   return a+b;
+//}
+
+// amal 1ta bolsa => bir qatorda blok sxemasiz yoziladi
+//console.log(CalculateXojiaka1(9,8));
+
+// metod -> tayyor funksiya => obyektda,arrayda,stringda =>
+
+  //const hojiaka='Jamiaat!'; // 12 ta belgi bor
+  //console.log(hojiaka.length);
+ 
+  // lenght -> metod -> property
+  // qiymat 
+  // metod  funksiya  () =>argument
+  //console.log('Bu harf',hojiaka.charAt(2));
+  //console.log(hojiaka[5]);  // property
+  //console.log(hojiaka[5]='A');
+  //console.log('Harflarni kattalashtrdik',uzbekistan.toUpperCase());
+ //console.log('harflarni kichraytirdik',hojiaka.toLowerCase());
+//console.log('kesish:',hojiaka.slice(0,-5));// -minus son 
+ //=> metod faqat return qiymat qaytaradi
+ //console.log('toza kesish',hojiaka.substring(0,-5));
+
+//  slice=> substring
+// console.log(hojiaka);
+
+// function powerOfFive(a) {
+//   return Math.pow(a, 5);
+// }
+
+// Misol uchun a = 2 deb olaylik
+//let a = 3;
+//console.log(`${a} ning 5-darajasi:`, powerOfFive(a));
+
+//function addNumbers(a, b) {
+  //return a + b; // a va b ning yig‘indisini qaytaradi
+//}
+
+//let result = addNumbers(5, 3); // 5 + 3 = 8
+//console.log(result); // 8 chiqadi
+
+//let word = "ONA"; // Asl matn
+//let arr = word.split(""); // Harflarni massivga aylantiramiz
+//arr[1] = "T"; // 1-indexdagi "N" ni "T" ga almashtiramiz
+//word = arr.join(""); // Massivni qayta stringga aylantiramiz
+
+//console.log(word); // "OTA" chiqadi
+
+//let text = "andijon vil";
+
+//let upperText = text.toUpperCase(); // Hammasini katta harfga o‘zgartirish
+//console.log(upperText); // "ANDIJON VIL"
+
+// let lowerText = text.toLowerCase(); // Hammasini kichik harfga o‘zgartirish
+// console.log(lowerText); // "andijon vil"
+
+// let text = "XUJAABAD";
+// let result = text.slice(1, 4); // 1-indexdan 4-indexgacha bo'lgan qismini olamiz
+
+// console.log(result); // "UJA"
+
+//silce -> substring
+
+
+const Password='     ozbekiston';
+// trim
+console.log(Password);
+console.log(Password.trim());
+console.log(Password.trimStart());
+// trimSart- oldidagi space chopadi
+console.log(Password.trimEnd());
+// trim methoddi bizga ortiqcha spacelarni qirqish uchun ishlatamiz
+
+// trimdi bizga ortiqcha spacelarni qirqish uchun ishlatamiz
+
+const son=13.4;
+console.log(son);
